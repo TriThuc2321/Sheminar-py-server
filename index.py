8f8901d34e9e2309cc7ad7557cc9914f21b5ed78
TriThuc2321 <19522321@gm.uit.edu.vn>
2022-10-05 00:44:36 +0700

    csv

--- index.py
+++ index.py
@@ -1,4 +1,5 @@
 import pandas as pd
 
-movies_df = pd.read_json("./dataset/draft.json")
+movies_df = pd.read_json("./dataset/movies.json")
+movies_df.to_csv("./dataset/movies.csv")
 print(movies_df)
