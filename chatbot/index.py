6cbd93d027825f0dfc2ffdc5461aac931940fc52
LAP01303 <19522321@gm.uit.edu.vn>
2022-10-31 23:12:44 +0700

    Create index.py

---
+++ chatbot/index.py
@@ -0,0 +1,22 @@
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+
+chatbot = ChatBot("Ron Obvious")
+
+
+conversation = [
+    "Hello",
+    "Hi there!",
+    "How are you doing?",
+    "I'm doing great.",
+    "That is good to hear",
+    "Thank you.",
+    "You're welcome."
+]
+
+trainer = ListTrainer(chatbot)
+
+trainer.train(conversation)
+
+response = chatbot.get_response("how are you")
+print(response)
